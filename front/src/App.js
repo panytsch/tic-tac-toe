555ce3cbba69e6da3e3849ddae2c248ea1fc384a
Panasiuk <gfyroman@gmail.com>
2018-12-07 21:41:00 +0200

    init

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div></div>
+    );
+  }
+}
+
+export default App;
